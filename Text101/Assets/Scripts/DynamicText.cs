e8af869d275c5e329e8d7aa77c13caa35cd2433c
Bhanu <uabhanu@gmail.com>
2017-07-29 19:53:39 +0100

    Project almost finished

--- Text101/Assets/Scripts/DynamicText.cs
+++ Text101/Assets/Scripts/DynamicText.cs
@@ -5,8 +5,10 @@ using UnityEngine.UI;
 
 public class DynamicText : MonoBehaviour 
 {
+    [SerializeField] Camera m_mainCamera;
     [SerializeField] Text m_textContainer;
     //Have different background & Text Colour for each state and finish your story going upto CV and finally, you get a Job or Game Success and you Win and they can start from beginning again
+    //Custom Colours WIP
 
     enum BhanuState
     {
@@ -31,6 +33,8 @@ public class DynamicText : MonoBehaviour
 
     void DoingNothing()
     {
+        m_mainCamera.backgroundColor = Color.black;
+        m_textContainer.color = Color.white;
         m_textContainer.text = "Bhanu's Story\n\n" + "Press J to make me start my Journey";
 
         if(Input.GetKeyDown(KeyCode.J))
@@ -41,6 +45,8 @@ public class DynamicText : MonoBehaviour
 
     void JourneyState()
     {
+        m_mainCamera.backgroundColor = Color.grey;
+        m_textContainer.color = Color.blue;
         m_textContainer.text = "Hi, I am Bhanu and I arrived to UK in Feb 2007 and since then, it has been ultimate struggle until 2017 just to get my Indefinite Leave " + 
                                "to Remain Visa (ILR). I am now completely focussed to settle in life and give me and my family a real lifestlyle which will make me fully " + 
                                "satisfied in life and I can rest at peace and one day, Rest In Peace \n\n" + "Press U to make me upgrade my skills or R to return to " +  
@@ -59,24 +65,15 @@ public class DynamicText : MonoBehaviour
 
     void UpdateStates()
     {
-        if(m_currentState == BhanuState.DoingNoting)
-        {
-            DoingNothing();
-        }
-
-        else if(m_currentState == BhanuState.Journey)
-        {
-            JourneyState();
-        }
-
-        else if(m_currentState == BhanuState.UpgradeSkills)
-        {
-            UpgradeSkills();
-        }
+             if(m_currentState == BhanuState.DoingNoting) {DoingNothing();}
+        else if(m_currentState == BhanuState.Journey) {JourneyState();}
+        else if(m_currentState == BhanuState.UpgradeSkills) {UpgradeSkills();}
     }
 
     void UpgradeSkills()
     {
+        m_mainCamera.backgroundColor = Color.white;
+        m_textContainer.color = Color.black;
         m_textContainer.text = "Man!!!! learning Programming is mind scrambler but since there is lot of money in the industry, I will do what it takes to become an excellent C# and/or " +
                                "C++ programmer and get a great job or even better, with the help of great Game engines like Unity3D with C# or Unreal with C++, make a great game and " + 
                                "make shit load of money so me and my family can have a great lifestyle\n\n" + "Press R to make me go back to my Journey State" ;
